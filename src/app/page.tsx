5ae5da05bd9040553ae40cf489d4078aaa6a4022
Zibran_Hasan <100549342+zibranhasan@users.noreply.github.com>
2024-09-27 10:56:43 +0600

    redux-persist implemented on auth state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
 export default function Home() {
+  const token = useSelector((state: RootState) => state.auth.token);
+  console.log(token);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>This is first page for course management system</h1>
--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { store } from "@/redux/store";
+import { persistor, store } from "@/redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}></PersistGate>
+      {children}
+    </Provider>
+  );
 };
 
 export default Providers;
